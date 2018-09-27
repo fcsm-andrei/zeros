35b1d8bcaf585dcd1cbd0a194cbc35303bacb984
fcsm-andrei <andreileonchikov@gmail.com>
2018-09-27 16:30:19 +0300

    task zero

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+	let i = 1,
+    result = 0;
+    while (number >= i){
+    	i *= 5;
+        result += Math.floor(number/i);
+    }
+        
+    return result;  
 }
+//const zerosCount = getZerosCount(100); // Factorial of 10 is 3628800
+    //console.log(zerosCount); // 2. Because there is 2 *tail* zeros in number 3628800
